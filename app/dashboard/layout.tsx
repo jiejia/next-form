6337784da503540730fd6682510e5c5bb66e8a52
jiejia <jiejia2009@gmail.com>
2024-04-18 15:02:41 +0800

    add profile nav

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -32,7 +32,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 <div className="navbar-end">
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
+                            <div className="w-10 rounded-full ring  ring-offset-1">
                                 <img alt="Tailwind CSS Navbar component"
                                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
                             </div>

f3045be50590fc3661b54302c74201c0eb3401ca
jiejia <jiejia2009@gmail.com>
2024-09-14 15:55:58 +0800

    a new sidebar created

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({children,}: { children: React.ReactNode
             <div className="fixed lg:left-[300px] left-0 top-0 right-0 p-4 grid grid-rows-[75px_1fr] h-full">
                 <header>
                     <Block className="">
-                        wegweg
+                        Dashboard
                     </Block>
                 </header>
                 <main className="h-full">

d52ab49f0682c7bc6d531f2fb4033250ab4eebe7
jiejia <jiejia2009@gmail.com>
2024-09-18 16:21:18 +0800

    header is designed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,15 @@
 import Block from "@/components/block";
 
 import Sidebar from "@/components/sidebar";
+import Header from "@/components/header";
+
 
 export default function DashboardLayout({children,}: { children: React.ReactNode }) {
     return (
         <div className="relative">
             <Sidebar/>
             <div className="fixed lg:left-[300px] left-0 top-0 right-0 p-4 grid grid-rows-[75px_1fr] h-full">
-                <header>
-                    <Block className="">
-                        Dashboard
-                    </Block>
-                </header>
+                <Header/>
                 <main className="h-full">
                     <Block className="h-full">
                         {children}
@@ -19,6 +17,5 @@ export default function DashboardLayout({children,}: { children: React.ReactNode
                 </main>
             </div>
         </div>
-
     )
 }

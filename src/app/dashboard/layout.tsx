5663f331f8b54a8dff917738449533718ecad95b
jiejia <jiejia2009@gmail.com>
2024-09-27 16:20:08 +0800

    design dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,5 @@
-import Block from "@/components/shared/block";
-
 import Sidebar from "@/components/ui/sidebar";
 import Header from "@/components/ui/header";
-
 
 export default function DashboardLayout({children,}: { children: React.ReactNode }) {
     return (
@@ -11,9 +8,7 @@ export default function DashboardLayout({children,}: { children: React.ReactNode
             <div className="fixed lg:left-[300px] left-0 top-0 right-0 p-4 grid grid-rows-[75px_1fr] h-full">
                 <Header/>
                 <main className="h-full">
-                    <Block className="h-full">
-                        {children}
-                    </Block>
+                    {children}
                 </main>
             </div>
         </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,34 @@
 'use client'
+import Block from '@/components/shared/block';
 
 
 export default function Dashboard() {
   return (
-    <div className="p-8">
-            
+    <div className="grid grid-flow-col gap-4">
+        <Block>
+          <h3 className="text-center">Number Of Forms</h3>
+          <div>
+              50
+          </div>
+        </Block>
+        <Block>
+          <h3 className="text-center">Number Of Submissions</h3>
+          <div>
+              99
+          </div>
+        </Block>
+        <Block>
+          <h3 className="text-center">Number Of Submissions</h3>
+          <div>
+            99
+          </div>
+        </Block>
+        <Block>
+          <h3 className="text-center">Number Of Submissions</h3>
+          <div>
+            99
+          </div>
+        </Block>
     </div>
   );
 }

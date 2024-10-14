6ce8a357b588486ae602ff27e7c7631c0f9bbfec
jiejia <jiejia2009@gmail.com>
2024-10-14 16:47:50 +0800

    form page

---
+++ src/app/dashboard/forms/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Block from '@/components/shared/block';
+
+
+export default function Forms() {
+    return (
+        <div className="grid grid-flow-col gap-4">
+
+        </div>
+    );
+}

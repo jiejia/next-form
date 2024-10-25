c248caee0a46be9ca47ec773142293f6f847eefc
jiejia <jiejia2009@gmail.com>
2024-10-25 16:40:37 +0800

    add the fields of form information to save-form page

--- src/components/ui/save-form.tsx
+++ src/components/ui/save-form.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import Block from '@/components/shared/block';
 import {Tabs, Tab} from "@nextui-org/react";
-import Image from "next/image";
+import ControlList from "@/components/ui/control-list";
+import {Input,Textarea,Select, SelectItem,Switch, cn} from "@nextui-org/react";
 
 export default function SaveForm() {
     const [selected, setSelected] = React.useState<string | number>("form");
@@ -19,38 +20,7 @@ export default function SaveForm() {
                     className=""
                 >
                     <Tab key="controls" title="Controls" className="px-0">
-                        <ul className="grid grid-cols-2 gap-2 text-left indent-1 text-xs content-start">
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/input_text.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Input Text</span>
-                            </li>
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/textarea.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Textarea</span>
-                            </li>
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/textarea.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Select</span>
-                            </li>
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/checkbox.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Checkbox</span>
-                            </li>
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/radio.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Radio</span>
-                            </li>
-                            <li className="p-2 bg-content2 rounded-lg border-default border-0 grid grid-cols-[20px_1fr]">
-                                <Image src="/svgs/date.svg" alt="Next Form" className="w-4 h-4" width={20}
-                                       height={20}/>
-                                <span>Date</span>
-                            </li>
-                        </ul>
+                        <ControlList/>
                     </Tab>
                 </Tabs>
             </Block>
@@ -67,10 +37,59 @@ export default function SaveForm() {
                     onSelectionChange={setSelected}
                 >
                     <Tab key="controls" title="Controls" className="px-0 xl:hidden block">
-
+                        <ControlList/>
                     </Tab>
-                    <Tab key="form" title="Form" className="px-0">
+                    <Tab key="form" title="Form" className="px-0 grid grid-flow-row gap-3">
+                        <Input
+                            isRequired
+                            type="text"
+                            label="Title"
+                            placeholder="Enter form title"
+                            className="max-w-xs"
+                            size="sm"
+                        />
+                        <Textarea
+                            label="Description"
+                            placeholder="Enter form description"
+                            className="max-w-xs"
+                            size="sm"
+                        />
+                        <Select
+                            label="Sequential numbering style"
+                            placeholder=""
+                            className="max-w-xs"
+                            defaultSelectedKeys={["None"]}
+                        >
+                                <SelectItem key="None">None</SelectItem>
+                                <SelectItem key="Arabic numerals">Arabic numerals</SelectItem>
 
+                        </Select>
+                        <Switch
+                            classNames={{
+                                base: cn(
+                                    "inline-flex flex-row-reverse w-full max-w-md bg-content2 hover:bg-content2 items-center",
+                                    "justify-between cursor-pointer rounded-lg gap-2 px-3 py-1 border-2 border-transparent",
+                                    "data-[selected=true]:border-primary",
+                                ),
+                                wrapper: "p-0 h-4 overflow-visible",
+                                thumb: cn("w-6 h-6 border-2 shadow-lg",
+                                    "group-data-[hover=true]:border-primary",
+                                    //selected
+                                    "group-data-[selected=true]:ml-6",
+                                    // pressed
+                                    "group-data-[pressed=true]:w-7",
+                                    "group-data-[selected]:group-data-[pressed]:ml-4",
+                                ),
+                            }}
+                            size="md"
+                        >
+                            <div className="flex flex-col gap-1">
+                                <p className="text-medium">Enable</p>
+                                <p className="text-tiny text-default-400">
+                                    Get access to new features .
+                                </p>
+                            </div>
+                        </Switch>
                     </Tab>
                     <Tab key="property" title="Property" className="px-0">
 

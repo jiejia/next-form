e5ede47ebc03eddf011748c7278b529acc0fbda4
jiejia <jiejia2009@gmail.com>
2025-02-17 16:54:42 +0800

    Refactor conditional rendering in SaveForm component for improved readability

--- src/components/ui/save-form.tsx
+++ src/components/ui/save-form.tsx
@@ -397,45 +397,31 @@ export default function SaveForm() {
                     </Button>
                 </Block>
                 <DragOverlay>
-                    {activeItem ? (
-                        activeItem.area == 'control' ? (
-                            controlItems[activeItem.id] ? (
-                                <Item
-                                    className="p-2 bg-content3 rounded-lg border-default border-0 grid grid-cols-[20px_1fr] z-40 text-xs"
-                                >
-                                <Image
-                                    src={
-                                        controlItems[activeItem.id].icon
-                                    }
-                                    alt="Next Form"
-                                    className="w-4 h-4"
-                                    width={20}
-                                    height={20}
-                                />
-                                <span>
-                                  {
-                                      controlItems[activeItem.id].type
-                                  }
-                                </span>
-                            </Item> ) : null
+                    {activeItem && (
+                        activeItem.area === 'control' ? (
+                            controlItems[activeItem.id] && (
+                                <Item className="p-2 bg-content3 rounded-lg border-default border-0 grid grid-cols-[20px_1fr] z-40 text-xs">
+                                    <Image
+                                        src={controlItems[activeItem.id].icon}
+                                        alt="Next Form"
+                                        className="w-4 h-4"
+                                        width={20}
+                                        height={20}
+                                    />
+                                    <span>{controlItems[activeItem.id].type}</span>
+                                </Item>
+                            )
                         ) : (
-                            elementItems[activeItem.id] ? (
-                                <Item
-                                    className="p-2 bg-content3 rounded-lg border-default border-0 relative z-40 text-xs"
-                                >
-                                <span className="text-sm">
-                                  {
-                                      elementItems[activeItem.id].title
-                                  }
-                                </span>
-                                <span className="absolute right-4 bottom-2 text-default-400">
-                                  {
-                                      elementItems[activeItem.id].type
-                                  }
-                                </span>
-                            </Item>
-                        ) : null )       
-                    ) : null}
+                            elementItems[activeItem.id] && (
+                                <Item className="p-2 bg-content3 rounded-lg border-default border-0 relative z-40 text-xs">
+                                    <span className="text-sm">{elementItems[activeItem.id].title}</span>
+                                    <span className="absolute right-4 bottom-2 text-default-400">
+                                        {elementItems[activeItem.id].type}
+                                    </span>
+                                </Item>
+                            )
+                        )
+                    )}
                 </DragOverlay>
             </DndContext>
         </div>

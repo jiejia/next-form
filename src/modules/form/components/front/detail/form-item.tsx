6ff88b21d7a27f749c12dcd9986cb85a9384d2cb
jiejia2009 <jiejia2009@gmail.com>
2025-05-19 15:29:26 +0800

    Refactor FormItem component to remove unused ControlComponentProps interface and streamline props structure

--- src/modules/form/components/front/detail/form-item.tsx
+++ src/modules/form/components/front/detail/form-item.tsx
@@ -1,12 +1,8 @@
 "use client"
 
 import React from "react";
-import dynamic from 'next/dynamic';
 import {Field} from "@/modules/form/types/form";
 
-interface ControlComponentProps {
-    field: Field;
-}
 
 export default  function FormItem (props: { field: Field, index: number, component: any }) {
 

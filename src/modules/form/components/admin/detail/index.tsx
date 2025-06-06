a42278cb643c5f54c423a989d621ecf836686f42
jiejia <jiejia2009@gmail.com>
2025-06-06 17:23:30 +0800

    Add grid layout and block components to index; enhance structure and organization of the form display

--- src/modules/form/components/admin/detail/index.tsx
+++ src/modules/form/components/admin/detail/index.tsx
@@ -1,10 +1,34 @@
 import { Form } from "@prisma/client";
+import Block from "@/modules/common/components/shared/block";
+
 
 export default function Index({form}: {form: Form}) {
 
     return (
-        <div>
-            
+        <div className="grid grid-rows-[1fr_1fr_4fr_56px] gap-4 h-full">
+            <Block>
+ewg
+            </Block>
+            <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-4">
+                <Block>
+                    wfq
+                </Block>
+                <Block>
+                    wfq
+                </Block>
+                <Block>
+                    wfq
+                </Block>
+                <Block>
+                    wfq
+                </Block>
+            </div>
+            <Block>
+weg
+            </Block>
+            <Block>
+egw
+            </Block>
         </div>
     );
 }
